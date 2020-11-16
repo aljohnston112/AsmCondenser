1825131fc7bed1f2c00e6184f4a7194ded790b64
Al <aljohnston112@gmail.com>
2020-11-16 17:09:16 -0600

    Splitting args and adding new instructions

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,9 +11,9 @@ import java.util.Scanner;
 
 public class Main {
 
-	private static File fileIn = new File("C:\\Users\\aljoh\\Documents\\Programming\\TEST\\engine.asm");
-	// private static File fileIn = new
-	// File("C:\\Users\\aljoh\\Documents\\Programming\\TEST\\test.txt");
+	 private static File fileIn = new
+	 File("C:\\Users\\aljoh\\Documents\\Programming\\TEST\\engine.asm");
+	//private static File fileIn = new File("C:\\Users\\aljoh\\Documents\\Programming\\TEST\\test.txt");
 
 	public static void main(String[] args) {
 		condenseAsm(fileIn);
@@ -66,14 +66,22 @@ public class Main {
 			}
 			if (lineScanner.hasNext()) {
 				lineArgs = lineScanner.nextLine();
-				String[] args = lineArgs.split(",");
-				if(args.length > 0){
-					arg1 = args[0].split(";")[0];
-					arg1 = arg1.trim();
-				}
-				if(args.length > 1){
-					arg2 = args[1].split(";")[0];
-					arg2 = arg2.trim();
+				String[] lineAndComment = lineArgs.split(";");
+				if (lineAndComment.length > 0) {
+					String[] args = lineAndComment[0].split(",");
+					if (lineAndComment.length > 1) {
+						fluff.add(" ");
+					}
+					for (int i = 1; i < lineAndComment.length; i++) {
+						fluff.add(";");
+						fluff.add(lineAndComment[i]);
+					}
+					if (args.length > 0) {
+						arg1 = args[0].trim();
+						if (args.length > 1) {
+							arg2 = args[1].trim();
+						}
+					}
 				}
 			}
 			if (arg1 != null && arg1.length() > 0) {
@@ -103,7 +111,10 @@ public class Main {
 					printTheRest = false;
 				} else if (lCInstruction.equals("push") || lCInstruction.equals("pop") || lCInstruction.equals("call")
 						|| lCInstruction.equals("jr") || lCInstruction.equals("jp") || lCInstruction.equals("ret")
-						|| lCInstruction.equals("bit")) {
+						|| lCInstruction.equals("bit") || lCInstruction.equals("res") || lCInstruction.equals("set")
+						|| lCInstruction.equals("db") || lCInstruction.equals("dw") || lCInstruction.equals("dl")
+						|| lCInstruction.equals("rept") || lCInstruction.equals("endr") || lCInstruction.equals("scf")
+						|| lCInstruction.equals("swap") || lCInstruction.equals("include")) {
 					out.write(line);
 					fluff.clear();
 					printTheRest = false;
@@ -122,6 +133,42 @@ public class Main {
 				} else if (lCInstruction.equals("add")) {
 					out.write(leadingWhiteSpace);
 					out.write(getADD(arg1, arg2));
+				}  else if (lCInstruction.equals("adc")) {
+					out.write(leadingWhiteSpace);
+					out.write(getADC(arg1, arg2));
+				} else if (lCInstruction.equals("sub")) {
+					out.write(leadingWhiteSpace);
+					out.write(getSUB(arg1, arg2));
+				} else if (lCInstruction.equals("sbc")) {
+					out.write(leadingWhiteSpace);
+					out.write(getSBC(arg1, arg2));
+				} else if (lCInstruction.equals("sla")) {
+					out.write(leadingWhiteSpace);
+					out.write(getSLA(arg1));
+				} else if (lCInstruction.equals("sra")) {
+					out.write(leadingWhiteSpace);
+					out.write(getSRA(arg1));
+				}  else if (lCInstruction.equals("srl")) {
+					out.write(leadingWhiteSpace);
+					out.write(getSRL(arg1));
+				} else if (lCInstruction.equals("rr")) {
+					out.write(leadingWhiteSpace);
+					out.write(getRR(arg1));
+				} else if (lCInstruction.equals("rl")) {
+					out.write(leadingWhiteSpace);
+					out.write(getRL(arg1));
+				} else if (lCInstruction.equals("rra")) {
+					out.write(leadingWhiteSpace);
+					out.write(getRRA());
+				} else if (lCInstruction.equals("rla")) {
+					out.write(leadingWhiteSpace);
+					out.write(getRLA());
+				} else if (lCInstruction.equals("rlca")) {
+					out.write(leadingWhiteSpace);
+					out.write(getRLCA());
+				} else if (lCInstruction.equals("rrca")) {
+					out.write(leadingWhiteSpace);
+					out.write(getRRCA());
 				} else if (lCInstruction.equals("and")) {
 					out.write(leadingWhiteSpace);
 					out.write(getAND(arg1, arg2));
@@ -149,19 +196,104 @@ public class Main {
 		out.flush();
 	}
 
+	private static String getSLA(String arg1) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if(arg1 != null) {
+		stringBuilder.append("(cf <- ");
+		stringBuilder.append(arg1);
+		stringBuilder.append(" <- 0)");
+		} else {
+			throw new IllegalArgumentException("Invalid SLA instruction");
+		}
+		return stringBuilder.toString();
+	}
+
+	private static String getSRA(String arg1) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if(arg1 != null) {
+		stringBuilder.append("(bit7 -> ");
+		stringBuilder.append(arg1);
+		stringBuilder.append(" -> cf)");
+		} else {
+			throw new IllegalArgumentException("Invalid SRA instruction");
+		}
+		return stringBuilder.toString();
+	}
+	
+	private static String getSRL(String arg1) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if(arg1 != null) {
+		stringBuilder.append("(0 -> ");
+		stringBuilder.append(arg1);
+		stringBuilder.append(" -> cf)");
+		} else {
+			throw new IllegalArgumentException("Invalid SRL instruction");
+		}
+		return stringBuilder.toString();
+	}
+	
+	
+	private static String getRLCA() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("(cf <- a <- bit7)");
+		return stringBuilder.toString();
+	}
+	
+	private static String getRRCA() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("(bit0 -> a -> cf)");
+		return stringBuilder.toString();
+	}
+	
+	private static String getRRA() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("(cf -> a -> cf)");
+		return stringBuilder.toString();
+	}
+	
+	private static String getRLA() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("(cf <- a <- cf)");
+		return stringBuilder.toString();
+	}
+	
+	private static String getRR(String arg1) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if(arg1 != null) {
+		stringBuilder.append("(cf -> ");
+		stringBuilder.append(arg1);
+		stringBuilder.append(" -> cf)");
+		} else {
+			throw new IllegalArgumentException("Invalid RR instruction");
+		}
+		return stringBuilder.toString();
+	}
+	
+	private static String getRL(String arg1) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if(arg1 != null) {
+		stringBuilder.append("(cf <- ");
+		stringBuilder.append(arg1);
+		stringBuilder.append(" <- cf)");
+		} else {
+			throw new IllegalArgumentException("Invalid RL instruction");
+		}
+		return stringBuilder.toString();
+	}
+	
 	private static String getCP(String arg1, String arg2) {
 		StringBuilder stringBuilder = new StringBuilder();
 		if (arg1 != null) {
 			if (arg2 != null) {
-				stringBuilder.append("c if (");
+				stringBuilder.append("cf if (");
 				stringBuilder.append(arg1);
 				stringBuilder.append(" < ");
 				stringBuilder.append(arg2);
-				stringBuilder.append(") ");
+				stringBuilder.append(")");
 			} else {
-				stringBuilder.append("c if (a < ");
+				stringBuilder.append("cf if (a < ");
 				stringBuilder.append(arg1);
-				stringBuilder.append(") ");
+				stringBuilder.append(")");
 			}
 		} else {
 			throw new IllegalArgumentException("Invalid cp instruction");
@@ -197,33 +329,108 @@ public class Main {
 			if (arg2 != null) {
 				stringBuilder.append(arg1);
 				stringBuilder.append(" = ");
+				stringBuilder.append("(");
 				stringBuilder.append(arg1);
 				stringBuilder.append(" + ");
 				stringBuilder.append(arg2);
+				stringBuilder.append(")");
 			} else {
-				stringBuilder.append("a = a");
+				stringBuilder.append("a = (a");
 				stringBuilder.append(" + ");
 				stringBuilder.append(arg1);
+				stringBuilder.append(")");
 			}
 		} else {
 			throw new IllegalArgumentException("Invalid add instruction");
 		}
 		return stringBuilder.toString();
 	}
+	
+	private static String getADC(String arg1, String arg2) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if (arg1 != null) {
+			if (arg2 != null) {
+				stringBuilder.append(arg1);
+				stringBuilder.append(" = ");
+				stringBuilder.append("(");
+				stringBuilder.append(arg1);
+				stringBuilder.append(" + ");
+				stringBuilder.append(arg2);
+				stringBuilder.append(" + cf)");
+			} else {
+				stringBuilder.append("a = (a");
+				stringBuilder.append(" + ");
+				stringBuilder.append(arg1);
+				stringBuilder.append(" + cf)");
+			}
+		} else {
+			throw new IllegalArgumentException("Invalid adc instruction");
+		}
+		return stringBuilder.toString();
+	}
+	
+	private static String getSUB(String arg1, String arg2) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if (arg1 != null) {
+			if (arg2 != null) {
+				stringBuilder.append(arg1);
+				stringBuilder.append(" = ");
+				stringBuilder.append("(");
+				stringBuilder.append(arg1);
+				stringBuilder.append(" - ");
+				stringBuilder.append(arg2);
+				stringBuilder.append(")");
+			} else {
+				stringBuilder.append("a = (a");
+				stringBuilder.append(" - ");
+				stringBuilder.append(arg1);
+				stringBuilder.append(")");
+			}
+		} else {
+			throw new IllegalArgumentException("Invalid sub instruction");
+		}
+		return stringBuilder.toString();
+	}
 
+	private static String getSBC(String arg1, String arg2) {
+		StringBuilder stringBuilder = new StringBuilder();
+		if (arg1 != null) {
+			if (arg2 != null) {
+				stringBuilder.append(arg1);
+				stringBuilder.append(" = ");
+				stringBuilder.append("(");
+				stringBuilder.append(arg1);
+				stringBuilder.append(" - ");
+				stringBuilder.append(arg2);
+				stringBuilder.append(" - cf)");
+			} else {
+				stringBuilder.append("a = (a");
+				stringBuilder.append(" - ");
+				stringBuilder.append(arg1);
+				stringBuilder.append(" - cf)");
+			}
+		} else {
+			throw new IllegalArgumentException("Invalid sbc instruction");
+		}
+		return stringBuilder.toString();
+	}
+
+	
 	private static String getXOR(String arg1, String arg2) {
 		StringBuilder stringBuilder = new StringBuilder();
 		if (arg1 != null) {
 			if (arg2 != null) {
 				stringBuilder.append(arg1);
 				stringBuilder.append(" = ");
+				stringBuilder.append("(");
 				stringBuilder.append(arg1);
 				stringBuilder.append(" ^ ");
 				stringBuilder.append(arg2);
 			} else {
-				stringBuilder.append("a = a");
+				stringBuilder.append("a = (a");
 				stringBuilder.append(" ^ ");
 				stringBuilder.append(arg1);
+				stringBuilder.append(")");
 			}
 		} else {
 			throw new IllegalArgumentException("Invalid xor instruction");
@@ -257,13 +464,16 @@ public class Main {
 			if (arg2 != null) {
 				stringBuilder.append(arg1);
 				stringBuilder.append(" = ");
+				stringBuilder.append("(");
 				stringBuilder.append(arg1);
 				stringBuilder.append(" & ");
 				stringBuilder.append(arg2);
+				stringBuilder.append(")");
 			} else {
-				stringBuilder.append("a = a");
+				stringBuilder.append("a = (a");
 				stringBuilder.append(" & ");
 				stringBuilder.append(arg1);
+				stringBuilder.append(")");
 			}
 		} else {
 			throw new IllegalArgumentException("Invalid and instruction");
